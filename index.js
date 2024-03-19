2df297950e5d6874f89e04e9c8c35dc24f6fbbce
whitney shisia <shisiawhitney215@gmail.com>
2024-03-19 19:33:00 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function(){
+return 20
+}
+const square = (parameter1) => parameter1 ** parameter1;
+square (2 , 2); 
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3); //=> 5
